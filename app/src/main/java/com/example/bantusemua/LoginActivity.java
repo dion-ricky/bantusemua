51c3fdb7d7c2d6f204f4c5f5b5edab34ba181596
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-12-16 15:24:04 +0700

    🔥 Implemented Google Auth

--- app/src/main/java/com/example/bantusemua/LoginActivity.java
+++ app/src/main/java/com/example/bantusemua/LoginActivity.java
@@ -13,10 +13,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,6 +34,9 @@ public class LoginActivity extends AppCompatActivity {
     EditText tfEmail, tfPassword;
     ImageButton btnLoginGoogle, btnLoginFacebook, btnMasuk;
     Button btnGoToRegister;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private int RC_GOOGLE_SIGN_IN = 90;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,9 +52,26 @@ public class LoginActivity extends AppCompatActivity {
         btnGoToRegister = findViewById(R.id.btnGoToRegister);
         btnMasuk = findViewById(R.id.btnMasuk);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         btnGoToRegister.setOnClickListener(new goToRegister());
         btnLoginGoogle.setOnClickListener(new handleLoginGoogle());
         btnMasuk.setOnClickListener(new handleEmailPassLogin());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            goToHome();
+        }
     }
 
     class handleEmailPassLogin implements View.OnClickListener {
@@ -59,9 +88,7 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Intent home = new Intent(LoginActivity.this, MainActivity.class);
-                                startActivity(home);
-                                LoginActivity.this.finish();
+                                goToHome();
                             } else {
                                 Toast.makeText(LoginActivity.this, "Login gagal!", Toast.LENGTH_SHORT).show();
                             }
@@ -73,8 +100,43 @@ public class LoginActivity extends AppCompatActivity {
     class handleLoginGoogle implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-
+            Intent googleSignIn = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(googleSignIn, RC_GOOGLE_SIGN_IN);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("AUTH_GOOGLE", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.w("AUTH_GOOGLE", "Google sign in failed", e);
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("AUTH_GOOGLE", "signInWithCredential:success");
+                            goToHome();
+                        } else {
+                            Log.w("AUTH_GOOGLE", "signInWithCredential:failure", task.getException());
+                        }
+                    }
+                });
     }
 
     class goToRegister implements View.OnClickListener {
@@ -83,5 +145,11 @@ public class LoginActivity extends AppCompatActivity {
             Intent openRegister = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(openRegister);
         }
+    }
+
+    private void goToHome() {
+        Intent home = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(home);
+        LoginActivity.this.finish();
     }
 }
