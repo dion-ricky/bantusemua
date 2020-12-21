10ebd0e692bb8e480e1d756b8dd7b036ec02cc16
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-12-21 08:25:54 +0700

    🔥 Yooo... realtimedb is working babyy

--- app/src/main/java/com/example/bantusemua/MainActivity.java
+++ app/src/main/java/com/example/bantusemua/MainActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.bantusemua.DbRepo.Donasi;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView ivAvatar = findViewById(R.id.imageView11);
         ivAvatar.setOnClickListener(new logout());
+    }
 
+    void insertToDb(String judul, String lokasi, String pelaksana, String kategori, Double target, Double terkumpul) {
+        Donasi donasi = new Donasi(
+                judul, lokasi, pelaksana, kategori, target, terkumpul
+        );
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("donasi").push().setValue(donasi);
     }
 
     class logout implements View.OnClickListener {
