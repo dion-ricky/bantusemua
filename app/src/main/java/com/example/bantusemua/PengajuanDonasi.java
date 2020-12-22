2785936932b200f7f3ac172f6fc141f74b459d65
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-12-23 05:42:59 +0700

    🍣 Prepare for Firebase Storage

---
+++ app/src/main/java/com/example/bantusemua/PengajuanDonasi.java
@@ -0,0 +1,14 @@
+package com.example.bantusemua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PengajuanDonasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengajuan_donasi);
+    }
+}
